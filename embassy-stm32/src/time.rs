cbd5f39f2b1aadeafb0eb768aceac6bad51792ed
Yann Diorcet <diorcet.yann@gmail.com>
2025-06-12 10:17:19 +0200

    MaybeHertz: set underling public

--- embassy-stm32/src/time.rs
+++ embassy-stm32/src/time.rs
@@ -96,7 +96,7 @@ impl Div<Hertz> for Hertz {
 /// An `Option<Hertz>` can be `.into()`'d into this type and back.
 /// The only restriction is that that [Hertz] cannot have the value 0 since that's
 /// seen as the `None` variant.
-pub struct MaybeHertz(u32);
+pub struct MaybeHertz(pub u32);
 
 impl MaybeHertz {
     /// Same as calling the `.into()` function, but without type inference.
